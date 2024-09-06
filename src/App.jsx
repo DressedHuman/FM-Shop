34b7e6cad0e45c6f860b72762e172df130903f68
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-06 15:50:05 +0600

    initialized the project with react, react-router-dom, tailwindcss installed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <>
+      <h2 className='text-3xl font-semibold text-black text-center my-7'>Bismillahir Rahmanir Rahim</h2>
+    </>
+  )
+}
+
+export default App
