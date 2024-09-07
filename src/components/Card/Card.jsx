050e42d4ed927a5b352efe8d28159f3ec99c54ee
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-08 05:26:28 +0600

    implemented addToCart and removeFromCart handlers

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,53 @@
 import PropTypes from 'prop-types';
 import Cart2 from '../../assets/cart2.svg';
+import { useContext, useEffect, useState } from 'react';
+import { CartContext } from '../Contexts';
 
-const Card = ({productInfo}) => {
+const Card = ({ productInfo, addToCart, removeFromCart }) => {
+    const { cart } = useContext(CartContext);
+    const [addedToCart, setAddedToCart] = useState(false);
+
+    const handleAddToCart = () => {
+        setAddedToCart(true);
+        addToCart(productInfo.id);
+    }
+
+    const handleRemoveFromCart = () => {
+        setAddedToCart(false);
+        removeFromCart(productInfo.id);
+    }
+
+
+    // initial checking if added or not
+    useEffect(() => {
+        setAddedToCart(cart.products.includes(productInfo.id));
+    }, [])
+
     return (
         <div className='p-3 flex flex-col rounded-lg shadow-lg'>
             <img className='w-full aspect-square object-contain' src={productInfo.image} alt={productInfo.title} />
-            <h2 className='text-lg text-[#343434] font-barlow font-semibold my-4'>{productInfo.title.length>42 ? productInfo.title.slice(0, 42) : productInfo.title}</h2>
+            <h2 className='text-lg text-[#343434] font-barlow font-semibold mt-4 mb-1'>{productInfo.title.length > 42 ? productInfo.title.slice(0, 42) : productInfo.title}</h2>
             <h2 className='text-lg text-[#343434] font-barlow font-bold'>${productInfo.price}</h2>
-            <p className='text-[#838383] text-sm font-barlow flex-grow mt-4 mb-7'>{productInfo.description.length>115 ? productInfo.description.slice(0, 115) : productInfo.description}</p>
-            <button className='bg-[#202020] text-white font-barlow font-semibold flex justify-center items-center gap-3 w-full p-2 rounded-lg'>
-                <img className='w-[20px]' src={Cart2} alt="cart2 icon" />
-                <span>Add to cart</span>
-            </button>
+            <p className='text-[#838383] text-sm font-barlow flex-grow mt-1 mb-4'>{productInfo.description.length > 115 ? productInfo.description.slice(0, 115) : productInfo.description}</p>
+
+            {
+                addedToCart ?
+                    <button
+                        onClick={handleRemoveFromCart}
+                        className='bg-[green] text-white font-barlow font-semibold flex justify-center items-center gap-3 w-full p-2 rounded-lg'
+                    >
+                        <img className='w-[20px]' src={Cart2} alt="cart2 icon" />
+                        <span>Remove from cart</span>
+                    </button>
+                    :
+                    <button
+                        onClick={handleAddToCart}
+                        className='bg-[#202020] text-white font-barlow font-semibold flex justify-center items-center gap-3 w-full p-2 rounded-lg'
+                    >
+                        <img className='w-[20px]' src={Cart2} alt="cart2 icon" />
+                        <span>Add to cart</span>
+                    </button>
+            }
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -34,8 +34,30 @@ const Home = () => {
     // sort products by category
     const sort = (category) => {
         setSelectedCategory(category);
-        const _products = products.filter(product => product.category===category);
+        const _products = products.filter(product => product.category === category);
         setShowingProducts(_products);
+    }
+
+    // add to cart handler
+    const addToCart = (productId) => {
+        const product = products.find(product => product.id === productId);
+        setCart({
+            totalProducts: cart.totalProducts + 1,
+            products: [...cart.products, product.id],
+            totalPrice: cart.totalPrice + product.price,
+        });
+    }
+
+    // remove from cart handler
+    const removeFromCart = (productId) => {
+        const product = products.find(product => product.id === productId);
+        if (cart.products.includes(product.id)) {
+            setCart({
+                totalProducts: cart.totalProducts - 1,
+                products: cart.products.filter(product => !(product.id === productId)),
+                totalPrice: cart.totalPrice - product.price,
+            });
+        }
     }
 
     return (
@@ -47,15 +69,20 @@ const Home = () => {
                         categories.map((category, idx) => <button
                             onClick={() => sort(category)}
                             key={idx}
-                            className={`w-full text-start ${selectedCategory===category ? "bg-black text-white" : "bg-white text-[#717171]"} font-semibold text-[22px] rounded-lg px-4 py-2`}
+                            className={`w-full text-start ${selectedCategory === category ? "bg-black text-white" : "bg-white text-[#717171]"} font-semibold text-[22px] rounded-lg px-4 py-2`}
                         >
                             {category}
                         </button>)
                     }
                 </div>
-                <div className="col-span-3 grid grid-cols-3 gap-4">
+                <div className="col-span-3 grid grid-cols-3 gap-7">
                     {
-                        showingProducts.map(product => <Card key={product.id} productInfo={product} />)
+                        showingProducts.map(product => <Card
+                            key={product.id}
+                            productInfo={product}
+                            addToCart={addToCart}
+                            removeFromCart={removeFromCart}
+                        />)
                     }
                 </div>
             </div>
