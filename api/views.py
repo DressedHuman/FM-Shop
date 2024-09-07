ab9c03e1f052043194a12f62cc25a7cf5893e629
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-07 10:40:31 +0600

    signup view implemented

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from user.models import CustomUser
+from user.serializers import CustomUserSerializer
+
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def signup(req):
+    serializer = CustomUserSerializer(data=req.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = CustomUser.objects.get(email=req.data["email"])
+        user.set_password(req.data["password"])
+        user.save()
+        return Response({"detail": "Signup Successful!"}, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
