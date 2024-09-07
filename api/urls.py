515a554da697736016891f63d19a8eae8686f1d8
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-09-07 21:54:51 +0600

    implemented get_categories view

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('user/signup/', signup, name="signup"),
     path('user/login/', login, name="login"),
     path('user/test_token/', test_token, name="test_token"),
+    path('categories/', get_categories, name="categories"),
     path('products/', get_products, name="products"),
 ]
--- api/views.py
+++ api/views.py
@@ -7,8 +7,7 @@ from rest_framework.authtoken.models import Token
 
 from user.models import CustomUser
 from user.serializers import CustomUserSerializer
-from products import all_products
-
+from products import all_products, categories
 
 
 @api_view(["POST"])
@@ -25,27 +24,34 @@ def signup(req):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def login(req):
     try:
         user = CustomUser.objects.get(email=req.data["email"])
         if not user.check_password(req.data["password"]):
-            return Response({"detail": "Wrong Password"}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response(
+                {"detail": "Wrong Password"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
         token, created = Token.objects.get_or_create(user=user)
         serializer = CustomUserSerializer(instance=user)
-        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
+        return Response(
+            {"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK
+        )
     except:
         return Response({"detail": "Wrong Email"}, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 @api_view(["GET"])
 def test_token(req):
     return Response({"detail": "passed"}, status=status.HTTP_200_OK)
 
+
+# all categories api
+@api_view(["GET"])
+def get_categories(req):
+    return Response(categories, status=status.HTTP_200_OK)
 
 
 # all products api
